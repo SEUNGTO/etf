e3af56506a69ab13e2359e1bcb05905208f8770e
SEUNGTO <tmdduq9008@gmail.com>
2024-05-20 23:30:01 +0900

    commit -a

--- app.py
+++ app.py
@@ -101,8 +101,8 @@ if __name__ == '__main__' :
 
     header()
     inputData()
-
-    if code != "" and bas_dt != "" and target_date != "" :
+    confirm = st.button('확인')
+    if confirm :
         st.write('# start')
         with st.spinner('데이터를 불러오는 중입니다.'):
             changes = findChange(bas_dt, target_date, codeList, resultDict)

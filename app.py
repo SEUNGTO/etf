a726ab8cd07e86482a3e7647986808cae46f235c
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 08:28:24 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 conn = st.connection('mysql', type='sql')
 
+st.title('ETF 검색기')
+st.write('테스트 기간/ETF는 466810, 457930, 445690만 검색 가능')
 etf_code = st.text_input('ETF코드를 입력해주세요.')
 if st.button('검색'):
 # Perform query.

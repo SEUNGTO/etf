361153a1c809351d04f81350f6f32bc469da3a63
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 01:20:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import streamlit as st
 conn = st.connection('mysql', type='sql')
 
 # Perform query.
-df = conn.query('SELECT * from tb_etf;', ttl=600)
+df = conn.query('SELECT * from etf_20240518;', ttl=600)
 
 # Print results.
-st.write(df)
-for row in df.itertuples():
-    st.write(f"{row}")
+st.dataframe(df.head())
+# st.write(df)
+# for row in df.itertuples():
+#     st.write(f"{row}")

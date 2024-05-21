66afc9f6aedb6b3b4d1a4da6a33cc70601f4a11d
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 08:04:58 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import streamlit as st
 
 conn = st.connection('mysql', type='sql')
 
+etf_code = st.text_input('ETF코드를 입력해주세요.')
+if st.button('검색'):
 # Perform query.
-df = conn.query('SELECT * from etf_20240518;', ttl=600)
+
+    df = conn.query(f'SELECT * from etf_20240518 where eft_code = {etf_code};', ttl=600)
 
 # Print results.
-st.dataframe(df)
+    st.dataframe(df)

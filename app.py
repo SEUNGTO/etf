66e5803cf80d5f8f5f1926b64992aefbb546e59d
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-21 20:53:37 +0900

    test app.py

--- app.py
+++ app.py
@@ -1,118 +1,52 @@
-# http://172.30.1.57:8501/
-
 import streamlit as st
-import requests
-import pandas as pd
-import io
+import mysql.connector
 
+# MySQL 연결
+def connect_to_mysql(host, user, password, database):
+    try:
+        connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        if connection.is_connected():
+            st.write("MySQL에 성공적으로 연결되었습니다.")
+            return connection
+    except Exception as e:
+        st.write("MySQL 연결에 실패했습니다.", e)
+        return None
 
+# 데이터베이스에서 데이터 가져오기
+def fetch_data(connection, query):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        return data
+    except Exception as e:
+        st.write("데이터를 가져오는 데 실패했습니다.", e)
+        return None
 
-code = ""
-target_date = ""
-bas_dt = ""
-chgData = pd.DataFrame([])
-changes = pd.DataFrame([])
+# Streamlit 앱
+def main():
+    st.title("MySQL 데이터 조회")
 
-def header():
-    st.write("# ETF 검색")
+    # MySQL 연결 정보 입력
+    host = st.text_input("MySQL 호스트")
+    user = st.text_input("MySQL 사용자")
+    password = st.text_input("MySQL 비밀번호")
+    database = st.text_input("MySQL 데이터베이스")
 
+    if st.button("MySQL 연결"):
+        connection = connect_to_mysql(host, user, password, database)
+        if connection:
+            query = st.text_area("쿼리 입력")
+            if st.button("데이터 조회"):
+                data = fetch_data(connection, query)
+                if data:
+                    st.write("조회 결과:")
+                    st.write(data)
 
-def inputData():
-    global target_date, bas_dt, code
-    code = st.text_input("검색할 종목코드를 선택하세요.")
-    target_date = st.text_input("기준일자를 선택하세요(yyyymmdd형식).")
-    bas_dt = st.text_input("비교일자를 선택하세요(yyyymmdd형식).")
-
-
-@st.cache_data()
-def loadData():
-    url = 'https://raw.githubusercontent.com/SEUNGTO/botdata/main/resultDict.json'
-    response = requests.get(url)
-    resultDict = response.json()
-
-    for code in resultDict.keys():
-        for date in resultDict[code].keys():
-            resultDict[code][date] = pd.DataFrame(resultDict[code][date])
-
-    return resultDict
-
-@st.cache_resource()
-def codeListing():
-
-    url = 'https://raw.githubusercontent.com/SEUNGTO/botdata/main/codeList.json'
-    response = requests.get(url)
-    codeList = response.json()
-
-    return pd.DataFrame(codeList)
-
-@st.cache_data()
-def findChange(bas_dt, target_date, codeList, resultDict) :
-
-    global chgData
-
-    for idx, code in enumerate(codeList['단축코드']) :
-        try :
-            if idx == 0 :
-
-                basData = resultDict[code][bas_dt]
-                targetData = resultDict[code][target_date]
-
-                chgData = pd.merge(basData[['종목코드', '구성종목명','주식수(계약수)']],
-                                targetData[['종목코드', '주식수(계약수)']],
-                                how = 'left',
-                                on = '종목코드'
-                                )
-                chgData['변화분'] = chgData['주식수(계약수)_y'] - chgData['주식수(계약수)_x']
-                chgData.fillna(0, inplace = True)
-                chgData = chgData[chgData['변화분'] != 0][['종목코드', '변화분']]
-                chgData['ETF'] = code
-            else :
-                basData = resultDict[code][bas_dt]
-                targetData = resultDict[code][target_date]
-
-                tmp = pd.merge(basData[['종목코드', '구성종목명','주식수(계약수)']],
-                                targetData[['종목코드', '주식수(계약수)']],
-                                how = 'left',
-                                on = '종목코드'
-                                            )
-                tmp['변화분'] = tmp['주식수(계약수)_y'] - tmp['주식수(계약수)_x']
-                tmp.fillna(0, inplace = True)
-                tmp = tmp[tmp['변화분'] != 0][['종목코드','구성종목명', '변화분']]
-                tmp['ETF'] = code
-
-                chgData = pd.concat([chgData, tmp])
-        except :
-            continue
-
-
-    ETFcodeName = codeList[['단축코드', '한글종목약명']]
-    ETFcodeName.columns = ['ETF', 'ETF종목명']
-    chgData = pd.merge(chgData, ETFcodeName, how = 'left', on = 'ETF')
-    chgData = chgData[['ETF', 'ETF종목명', '종목코드', '구성종목명', '변화분']]
-
-    return chgData
-
-
-if __name__ == '__main__' :
-
-
-    codeList = codeListing()
-    codeList = codeList[(codeList['기초시장분류'] == '국내') & (codeList['기초자산분류'] == '주식')]
-    resultDict = loadData()
-
-    header()
-    inputData()
-
-    confirm = st.button('입력 완료')
-    # search = st.button('검색')
-
-    if confirm :
-        changes = findChange(bas_dt, target_date, codeList, resultDict)
-
-    # if search :
-        st.markdown('---------')
-        st.write('### 전체 내역')
-        st.dataframe(changes.sort_values('변화분', ascending = False))
-        st.markdown('---------')
-        st.write('### 해당 종목 내역')
-        st.dataframe(changes[changes['종목코드'] == code].sort_values('변화분', ascending = False))
+if __name__ == "__main__":
+    main()

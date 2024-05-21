36e60bbe785d251736956acde17974360c972f35
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 08:13:53 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ etf_code = st.text_input('ETF코드를 입력해주세요.')
 if st.button('검색'):
 # Perform query.
 
-    df = conn.query(f'SELECT * from etf_20240518 where eft_code = {etf_code};', ttl=600)
+    df = conn.query(f'SELECT * from etf_20240518 where etf_code = {etf_code};', ttl=600)
 
 # Print results.
     st.dataframe(df)

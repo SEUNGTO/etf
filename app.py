86d2a143d4ddceb79af9eef852db49a0e562a36d
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 08:26:20 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ if st.button('검색'):
     st.dataframe(df)
 
 stock_code = st.text_input('종목코드를 입력해주세요.')
-if st.button('검색') :
+if st.button('종목검색') :
     df = conn.query(f'SELECT * from etf_20240518 where stock_code = {stock_code};', ttl=600)
     st.dataframe(df)

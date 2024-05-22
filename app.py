7115449a8c8c9b494aeabb29e58e91ad7a79a50b
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 21:52:44 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -22,7 +22,9 @@ if st.button('검색'):
     df = df.loc[:, ['stock_code', 'stock_nm', 'stock_amt', 'evl_amt']]
     df.columns = ['종목코드', '종목명', '보유량', '평가금액']
     df['비중'] = df['평가금액'].astype(int)/df['평가금액'].astype(int).sum() * 100
-    fig = px.pie(df.head(10), values = '비중', names = '종목명', title = '상위 10개 종목의 비중')
+    ratio = df.sort_values('비중', ascending = False)[['종목명', '비중']].head(10)
+    ratio.loc['기타', :] = [100 - ratio['종목명'].sum(), '기타']
+    fig = px.pie(ratio, values = '비중', names = '종목명', title = '상위 10개 종목의 비중')
     fig.update_layout(template='plotly')
     st.plotly_chart(fig, use_container_width = True)
     st.dataframe(df.sort_values('평가금액', ascending = False))

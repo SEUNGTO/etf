994b91e5c24c0a1e01b366558334f574696e9935
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 13:21:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import streamlit as st
 conn = st.connection('mysql', type='sql')
 
 st.title('ETF 검색기')
-st.write('ETF는 466810, 457930, 445690만 검색 가능')
+st.write('ETF는 TIGER200(102110), KODEX200(069500)만 검색 가능')
 etf_code = st.text_input('ETF코드를 입력해주세요.')
 if st.button('검색'):
-# Perform query.
-
-    df = conn.query(f'SELECT * from etf_20240518 where etf_code = {etf_code};', ttl=600)
-
-    st.dataframe(df)
+    st.write('### 1. 보유 내역')
+    df = conn.query(f'SELECT * from etf_20240521 where etf_code = {etf_code};', ttl=600)
+    df = df.loc[:, ['stock_code', 'stock_nm', 'stock_amt', 'evl_amt']]
+    df.columns = ['종목코드', '종목명', '보유량', '평가금액']
+    st.dataframe(df.sort_values('평가금액', ascending = False))
 
 stock_code = st.text_input('종목코드를 입력해주세요.')
 if st.button('종목검색') :

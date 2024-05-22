d214d2adf817b85a59c5094b7c7076fc3756321a
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 23:42:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import FinanceDataReader as fdr
 import plotly.express as px
 import plotly.graph_objs as go
 import pandas as pd
-test = pd.DataFrame({'name' : ['naver'],  'url' : ['https://www.naver.com']})
+test = pd.DataFrame({'name' : ['naver'],  'url' : ['https://www.naver.com']})\
+st.markdown(test.to_html(render_links=True),unsafe_allow_html=True)
 st.dataframe(test)
 stocks = {'102110' : 'TIGER200', '069500' : 'KODEX 200', '463050' : 'timefolio K바이오액티브', '482030' : 'Koact 테크핵심소재공급망액티브', '385720' : 'timefolio Kstock 액티브'}
 

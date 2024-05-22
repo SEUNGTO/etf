b087529919b172a61a811a131638b6001f0eb657
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 18:51:02 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if st.button('검색'):
     
     plotData = fdr.DataReader(etf_code, start ='2024-04-20', end = '2024-05-22')
     fig = px.line(plotData['Close'])
-    fig.add_bar(plotData['Volume'] yaxis = 'volume')
+    fig.add_bar(plotData['Volume'], yaxis = 'volume')
     fig.update_layout(
         yaxis=dict(
             title='Close Price'),

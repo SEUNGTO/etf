863cce7f990a5a4cf31d9436f86c8d92ee78f4f6
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 14:10:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import FinanceDataReader as fdr
-import matplotlib.pyplot as plt
 stocks = {'102110' : 'TIGER200', '069500' : 'KODEX 200'}
 
 conn = st.connection('mysql', type='sql')
@@ -14,6 +13,7 @@ if st.button('검색'):
     df = conn.query(f'SELECT * from etf_20240521 where etf_code = {etf_code};', ttl=600)
     df = df.loc[:, ['stock_code', 'stock_nm', 'stock_amt', 'evl_amt']]
     df.columns = ['종목코드', '종목명', '보유량', '평가금액']
+    df['비중'] = df['평가금액']/df['평가금액].sum() * 100
     st.dataframe(df.sort_values('평가금액', ascending = False))
 
     st.write(f'### 2. {stocks[etf_code]}의 최근 한 달 주가 추이에요.')
@@ -24,10 +24,11 @@ if st.button('검색'):
     df2 = conn.query(f'SELECT * from etf_20240518 where etf_code = {etf_code};', ttl=600)
     df2 = df2.loc[:, ['stock_code', 'stock_nm', 'stock_amt', 'evl_amt']]
     df2.columns = ['종목코드', '종목명', '보유량', '평가금액']
-    tmp = df[['종목코드', '종목명', '평가금액']].set_index('종목코드').join(df2[['종목코드', '평가금액']].set_index('종목코드'), 
+    df2['비중'] = df2['평가금액']/df2['평가금액].sum()*100
+    tmp = df[['종목코드', '종목명', '비중']].set_index('종목코드').join(df2[['종목코드', '비중']].set_index('종목코드'), 
                                                                              how = 'inner', lsuffix = 'T', rsuffix = 'C')
-    tmp['차이'] = tmp['평가금액T'].astype(int) - tmp['평가금액C'].astype(int)
-    tmp.columns = ['종목명', '기준일 평가금액', '비교일 평가금액', '차이']
+    tmp['차이'] = tmp['비중T'].astype(int) - tmp['비중C'].astype(int)
+    tmp.columns = ['종목명', '기준일 비중', '비교일 비중', '차이']
 
     st.write(f'### 3. {stocks[etf_code]}는 최근 아래 종목의 비중을 늘렸어요.')
     st.dataframe(tmp[tmp['차이'] > 0].sort_values('차이', ascending = False).head(10))

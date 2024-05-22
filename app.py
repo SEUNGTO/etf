ac4ea6b50805c8371b50e1d5a7400bf0a6cffe83
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-22 23:48:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.graph_objs as go
 import pandas as pd
 test = pd.DataFrame({'name' : ['naver'],  'url' : ['https://www.naver.com']})
 st.dataframe(test, column_config={
-        "url": st.column_config.LinkColumn(label = 'link')
+        "url": st.column_config.LinkColumn(display_text = 'link')
     })
 stocks = {'102110' : 'TIGER200', '069500' : 'KODEX 200', '463050' : 'timefolio K바이오액티브', '482030' : 'Koact 테크핵심소재공급망액티브', '385720' : 'timefolio Kstock 액티브'}
 

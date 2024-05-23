f2713028e9e8b35bf0c40c1d5f633b19486b0e4b
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-23 15:58:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ if st.button('검색'):
     tmp3 = tmp3.set_index('종목코드')
     tmp3 = tmp3.join(target, how='left')
     tmp3['종가'] = tmp3['평가금액']/tmp3['보유량']
-    ind = tmp3['목표가(가중평균'] == np.nan
-    
+    ind = tmp3['목표가(가중평균)'] == np.nan
+
     # tmp3['가격'] = np.where(tmp3['목표가(가중평균)'] == np.nan, tmp3['종가'], tmp3['목표가(가중평균'])
     st.dataframe(tmp3.loc[ind, :])
 

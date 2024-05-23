d87a280bf574450288a3d593dba8335827fc61de
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-23 19:49:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ if search :
         tmp['목표가(가중평균)'] = round(tmp['목표가(가중평균)'])
         st.dataframe(tmp.drop(['종목코드','보유량','평가금액'], axis = 1).sort_values('비중', ascending=False).rename(columns = {'목표가(가중평균)':'목표가(wAvg)'}), column_config={
             "링크": st.column_config.LinkColumn(display_text='\U0001F517'),
-"리포트 제목" : st.column_config.TextColumn(width = 'middle')})
+"리포트 제목" : st.column_config.TextColumn(width = 'small')})
         st.write('\* wAvg : 가중평균')
 
     st.write(f'### 2. {stocks[etf_code]}의 최근 한 달 주가 추이에요.')

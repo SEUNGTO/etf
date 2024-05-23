156aca7ddd669c8e86222e073d2de7f2ed2d10aa
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-23 16:30:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ if st.button('검색'):
 
     fig.add_trace(
         go.Scatter(
-            x = price['Date'],
+            x = price['Date'].max(),
             y = text_pos,
             mode='text',
             text=[f'{target_price:.2f}'],  # 표시할 텍스트

07cd020973dff1e47fc01b4d4af1b3ff9759238a
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-23 19:13:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ st.write('- Koact 테크핵심소재공급망액티브(482030)')
 st.write('- timefolio Kstock 액티브(385720)')
 # etf_code = st.text_input('ETF코드를 입력해주세요.', value = '102110')
 etf_code = st.session_state['etf_code']
-search = st.button('검색', key = 'search')
 etf_code = st.text_input('ETF코드를 입력해주세요.', value = '102110')
+search = st.button('검색', key = 'search')
 
 if search :
     # 전체 내역 조회

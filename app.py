575d5da9aad087e02e975c12d34e4236726ecf45
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-23 16:02:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -86,6 +86,8 @@ if st.button('검색'):
     tmp3 = tmp3.join(target, how='left')
     tmp3['종가'] = tmp3['평가금액']/tmp3['보유량']
     tmp3['목표가(가중평균)'].fillna(tmp3['종가'], inplace = True)
+    sichong = sum(tmp3['목표가(가중평균)'] * tmp3['보유량'])/50000
+    st.write(sichong)
 
     # tmp3['가격'] = np.where(tmp3['목표가(가중평균)'] == np.nan, tmp3['종가'], tmp3['목표가(가중평균'])
     st.dataframe(tmp3)

bad1399260e05684fd1a4c7f6eb215c96795607e
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-25 02:45:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -139,12 +139,12 @@ if search :
         close = price['Close'].tail(1).values[0]
         high = price['High'].max()
         delta = close - high
-        st.metric(label = '종가(고점 대비)', value = f'{close:,}',  delta = f'{delta}')
+        st.metric(label = '종가(고점 대비)', value = f'{close:,}',  delta = f'{delta:,}')
     with col3 :
         high = price['High'].max()
         low = price['Low'].min()
         delta = high - low
-        st.metric(label = '최고점(저점 대비)', value = high, delta = f'{delta}')
+        st.metric(label = '최고점(저점 대비)', value = f'{high:,}', delta = f'{delta:,}')
         
     
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)

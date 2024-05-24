b8f26f6a80b2bcc4299f9d462249127804aba8b5
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-25 01:16:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ st.write('- timefolio Kstock 액티브(385720)')
 
 # 검색 키워드 입력 받기
 codeList = fdr.StockListing('ETF/KR')
+st.dataframe(codeList)
 st.session_state.search_result = codeList['Name'].tolist()
 # st.session_state.search_results = process.extract(keyword, codeList['Name'], limit=50)
 

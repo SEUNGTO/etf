c419a6c41d144e858ddb4b3328229e5bc9b52a17
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-25 01:23:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ stocks = {'102110': 'TIGER200', '069500': 'KODEX 200', '463050': 'timefolio K바
 
 conn = st.connection('mysql', type='sql')
 
-st.session_state['etf_code'] = st.selectbox("종목명을 검색해주세요", [st.session_state.search_results])
+st.session_state['etf_code'] = st.selectbox("종목명을 검색해주세요", [v for v in st.session_state.search_results])
 st.session_state['eft_code'] = codeList[codeList['Name'] == st.session_state.selected_stock]['Symbol'].values[0]
 
 st.session_state['search'] = st.button(label = '검색')

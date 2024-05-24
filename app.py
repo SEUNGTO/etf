d75121d932ed135e4564eff678bd4c421806816e
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-25 02:14:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.graph_objs as go
 import pandas as pd
 import re
 from fuzzywuzzy import process
-from streamlit_searchbar import st_searchbox
+from streamlit_searchbox  import st_searchbox
 
 st.set_page_config(
     page_title="ETFace",

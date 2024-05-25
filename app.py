0795374385576621e88f073e8994cb9f6cbf8a99
SEUNGTO <tmdduq9008@gmail.com>
2024-05-25 21:23:05 +0900

    commit -a

--- app.py
+++ app.py
@@ -286,15 +286,14 @@ elif search and type == 'Stock' :
 
     col1, col2 = st.columns(2)
     with col1 :
-        with st.container() :
-            st.write('this is container')
-
         st.write(f'### 📈 최근 {stocks[etf_code]}의 비중을 늘렸어요.')
         increase = tmp[tmp['차이'] > 0].sort_values('차이', ascending=False)
         st.write(f'총 **{len(increase)}**개의 ETF에서 비중을 늘렸어요.')
         st.dataframe(increase.head(10), use_container_width=True)
 
+
     with col2 :
+        st.markdown('<style>body{background-color: Blue;}</style>', unsafe_allow_html=True)
         st.write(f'### 📉 최근 {stocks[etf_code]}의 비중을 줄였어요.')
         decrease = tmp[tmp['차이'] > 0].sort_values('차이', ascending=False).head(10)
         st.write(f'총 **{len(decrease)}**개의 ETF에서 비중을 늘렸어요.')

29930e5808a682948c155981d8fe5d1753111e5d
SEUNGTO <tmdduq9008@gmail.com>
2024-05-25 22:02:33 +0900

    commit -a

--- app.py
+++ app.py
@@ -321,6 +321,6 @@ elif search and type == 'Stock' :
         st.write(f'**{len(new)}**개의 ETF에서 {stocks[etf_code]}를 모두 정리했어요.')
         st.write(f'- 평균 {drop["평가금액"].mean():,.0f}원만큼 팔았어요.')
         st.write(f'- 가장 크게 비중을 줄인 ETF는 {drop.index[drop["원래 비중"].argmax()]}의 {drop["원래 비중"].max():,.0f}원이에요.')
-        st.write(f'- 가장 큰 금액을 판 ETF는 {drop.index[drop["평가 비중"].argmax()]}의 {drop["평가금액"].max():,.0f}원이에요.')
+        st.write(f'- 가장 큰 금액을 판 ETF는 {drop.index[drop["평가금액"].argmax()]}의 {drop["평가금액"].max():,.0f}원이에요.')
 
         st.dataframe(drop, use_container_width=True)

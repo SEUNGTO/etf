e5f6036f3b1d5c10ea377727a9351b8be61cd0cb
SEUNGTO <tmdduq9008@gmail.com>
2024-05-25 21:35:03 +0900

    commit -a

--- app.py
+++ app.py
@@ -309,10 +309,10 @@ elif search and type == 'Stock' :
 
         # decrease = tmp[tmp['차이'] > 0].sort_values('차이', ascending=False).head(10)
         decrease = pd.DataFrame({'ETF' : ['TIGER 200', 'KODEX 300'],
-                                 '기준일 비중' : [50.00, 20.00],
-                                 '비교일 비중' : [30.00, 10.00],
-                                 '차이' : [-20.00, -10.00]})
-        decrease = decrease.style.applymap(lambda x : 'background-color : blue' if x < 0 else '', subset=['차이'])
+                                 '기준일 비중' : [50, 20],
+                                 '비교일 비중' : [30, 10],
+                                 '차이' : [-20, -10]})
+        decrease = decrease.style.apply(lambda x : 'background-color : blue' if x < 0 else '', subset=['차이'])
         st.dataframe(decrease)
-        st.write(f'총 **{len(decrease)}**개의 ETF에서 비중을 늘렸어요.')
-        st.dataframe(decrease.head(10), use_container_width=True,)
+        # st.write(f'총 **{len(decrease)}**개의 ETF에서 비중을 늘렸어요.')
+        # st.dataframe(decrease.head(10), use_container_width=True,)

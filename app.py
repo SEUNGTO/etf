54dd377f6f30026c3ecb3f291f7cfe46f2a10a44
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-26 23:22:01 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -242,16 +242,17 @@ elif search and type == 'Stock' :
     tab1, tab2, tab3, tab4 = st.tabs(['증권사 리포트', '뉴스', '텔레그램', '유튜브'])
 
     with tab1 :
+        try : 
+            tmp = research.set_index('종목명').drop(['종목코드', 'nid'], axis = 1).sort_values('게시일자', ascending = False)
+            st.write(f' 총 **{len(tmp["목표가"])}**개의 리포트가 있어요.')
 
-        tmp = research.set_index('종목명').drop(['종목코드', 'nid'], axis = 1).sort_values('게시일자', ascending = False)
-        st.write(f' 총 **{len(tmp["목표가"])}**개의 리포트가 있어요.')
-
-        st.write(f' 증권사의 평균 목표가는 **{tmp["목표가"].mean():,.0f}**원이에요.')
-        st.write(f'- 가장 높은 목표가는 {tmp[tmp["목표가"] == tmp["목표가"].max()]["증권사"].values[0]}의 {tmp["목표가"].max():,.0f}원이에요.')
-        st.write(f'- 가장 낮은 목표가는 {tmp[tmp["목표가"] == tmp["목표가"].min()]["증권사"].values[0]}의 {tmp["목표가"].min():,.0f}원이에요.')
-        st.dataframe(tmp.reset_index(drop=True), column_config= {'링크' : st.column_config.LinkColumn(display_text='\U0001F517')},
+            st.write(f' 증권사의 평균 목표가는 **{tmp["목표가"].mean():,.0f}**원이에요.')
+            st.write(f'- 가장 높은 목표가는 {tmp[tmp["목표가"] == tmp["목표가"].max()]["증권사"].values[0]}의 {tmp["목표가"].max():,.0f}원이에요.')
+            st.write(f'- 가장 낮은 목표가는 {tmp[tmp["목표가"] == tmp["목표가"].min()]["증권사"].values[0]}의 {tmp["목표가"].min():,.0f}원이에요.')
+            st.dataframe(tmp.reset_index(drop=True), column_config= {'링크' : st.column_config.LinkColumn(display_text='\U0001F517')},
                      use_container_width=True,
 hide_index = True)
+        except : st.error('증권사 리포트가 없어요.')
     with tab2 :
 
         st.write(f'**네이버 뉴스**에서 방금 {stocks[etf_code]}를 검색한 결과에요.')

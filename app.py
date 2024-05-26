0316c77d7011a24fe3f81caf3929d9f34ef73c14
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-26 23:31:02 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ codeList = pd.DataFrame({'Name' : ['TIGER 200', 'KODEX 200', 'timefolio K바이�
                          'Type' : ['ETF', 'ETF', 'ETF', 'ETF', 'ETF',
                                    'Stock', 'Stock', 'Stock', 'Stock', 'Stock']})
 
-codetmp = fdr.StockListing('KOSPI')
+codetmp = fdr.StockListing('KOSPI/KR')
 codetmp = codetmp[['Name', 'Symbol']]
 codetmp['Type'] = 'Stock'
 st.dataframe(codetmp)

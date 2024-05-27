ac140362dd0c25f9db7ca118c47ae6d7292b5a71
SEUNGTO <tmdduq9008@gmail.com>
2024-05-27 16:07:11 +0900

    commit -a

--- app.py
+++ app.py
@@ -194,6 +194,7 @@ elif search and type == 'Stock' :
 
 
     df = conn.query(f'SELECT * from etf_20240521 where stock_code = {code};', ttl=600)
+    st.dataframe(df)
     df = df.loc[:, ['code','stock_code', 'stock_nm', 'stock_amt', 'evl_amt']]
     df.columns = ['ETF코드','종목코드', '종목명', '보유량', '평가금액']
     df['비중'] = round(df['평가금액'].astype(int) / df['평가금액'].astype(int).sum() * 100, 2)

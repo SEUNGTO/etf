6ecdef0b17501696e5d823a289b692cf738de700
SEUNGTO <tmdduq9008@gmail.com>
2024-05-27 18:19:31 +0900

    commit -a

--- app.py
+++ app.py
@@ -115,6 +115,8 @@ if search and type == 'ETF':
     st.write(f'## 2. {name} 10개 종목과 관련된 이야기들이에요.')
 
     st.dataframe(ratio.drop('other'))
+    _top10 = radio.drop('other')['종목명']
+    st.dataframe(_top10)
 
     # for telegram, url in telegram_dict.items():
     #     with st.expander(f'{telegram}'):

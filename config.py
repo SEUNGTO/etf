af9e61224e16c95c8ae8a1559621313482db5e75
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-28 08:14:50 +0900

    config.py 업데이트

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ def set_session() :
 telegram_dict = {
     '주식 급등일보🚀급등테마·대장주 탐색기': 'https://t.me/s/FastStockNews'
     , 'AWAKE-실시간 주식 공시 정리채널': 'https://t.me/s/darthacking'
-    , 'AWAKE-일정, 테마, 이벤트드리븐' : 'https://t.me/awake_schedule'
+    , 'AWAKE-일정, 테마, 이벤트드리븐' : 'https://t.me/s/awake_schedule'
     # , '조정은 뇌를 뽑아가지': 'https://t.me/s/shlrkQhqgla'
     , '52주 신고가 모니터링' : 'https://t.me/s/awake_realtimeCheck'
     , 'SB 리포트 요약' : 'https://t.me/s/stonereport'

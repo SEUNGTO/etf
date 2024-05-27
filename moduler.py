d23fbe5288dca9fb0451e609f13e49cb99288c0b
SEUNGTO <tmdduq9008@gmail.com>
2024-05-27 17:12:26 +0900

    commit -a

--- moduler.py
+++ moduler.py
@@ -11,39 +11,48 @@ from bs4 import BeautifulSoup
 
 def telegram_crawller(url, keyword) :
     telegram_msgs = {
-            'msg': []
-            , 'link': []
-        }
-
-    query = f'{url}?q={keyword}'
-    response = requests.get(query)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
+        'msg': []
+        , 'date': []
+        , 'time': []
+        , 'link': []
+    }
     for msg in soup.find_all('div', class_='tgme_widget_message_bubble'):
 
         msg.find('a').decompose()
         try:
             msg = msg.find('div', class_='tgme_widget_message_text js-message_text').text
             telegram_msgs['msg'].append(msg)
+            datetime = pd.to_datetime(msg.find('time', class_='time').attrs['datetime'])
+            date = datetime.strftime('%Y-%m-%d')
+            time = datetime.strftime('%H:%M')
+
+            telegram_msgs['date'].append(date)
+            telegram_msgs['time'].append(time)
 
         except:
             msg = '(메세지없이 링크만 있어요.)'
             telegram_msgs['msg'].append(msg)
+
+            datetime = pd.to_datetime(msg.find('time', class_='time').attrs['datetime'])
+            date = datetime.strftime('%Y-%m-%d')
+            time = datetime.strftime('%H:%M')
+
+            telegram_msgs['date'].append(date)
+            telegram_msgs['time'].append(time)
 
     for uu in soup.find_all('a', class_='tgme_widget_message_date'):
         link = uu.attrs['href']
         telegram_msgs['link'].append(link)
 
     telegram_msgs = pd.DataFrame(telegram_msgs)
-    telegram_msgs.columns = ['메세지', '링크']
+    telegram_msgs.columns = ['메세지', '일자', '시간', '링크']
     return telegram_msgs
 
-# @st.cache_data
 def code_update(name, codeList) :
     st.session_state['code'] = codeList[codeList['Name'] == name]['Symbol'].values[0]
     st.session_state['type'] = codeList[codeList['Name'] == name]['Type'].values[0]
 
-# @st.cache_data
+@st.cache_data
 def load_codeList() :
     url = 'https://raw.githubusercontent.com/SEUNGTO/botdata/main/ETFcodeList.json'
     data = pd.DataFrame(requests.get(url).json())

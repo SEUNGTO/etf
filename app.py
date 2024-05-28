7a373f522613c4203762a96eab884889ef6a5f4f
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-28 16:50:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,12 @@ type = st.session_state['type']
 conn = st.connection('mysql', type='sql')
 
 if search and type == 'ETF':
+    if code == '102110' :
+        url = 'https://raw.githubusercontent.com/sEUNGTO/etfdata/main/tiger200.json'
+        response = requests.get(url).json()
+        st.dataframe(response)
+        
+        # {"\ubd80\ucc44\ucd1d\uacc4":5793815076.3636026382,"\uc790\ubcf8\ucd1d\uacc4":1994987871.0290408134,"\uc790\uc0b0\ucd1d\uacc4":7788802947.3926439285}
 
     # 전체 내역 조회
     df = load_etf_data('new', code)

702687a55c72abf049c0cf9a107e29f37ae2bca0
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-30 10:51:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -351,7 +351,8 @@ elif search and type == 'Stock' :
     tmp = df[['ETF코드', '종목명', '비중']].set_index('ETF코드').join(df2[['ETF코드', '비중']].set_index('ETF코드'),
                                                            how='left', lsuffix='T', rsuffix='C')
     st.dataframe(tmp)
-    tmp['차이'] = tmp['비중T'] - tmp['비중C']
+    tmp['차이'] = tmp['비중T'].astype(float) - tmp['비중C'].astype(float)
+    tmp['차이'] = round(tmp['차이'], 2)
     tmp = tmp.join(codeList[['Name', 'Symbol']].rename(columns = {'Symbol' : 'ETF코드', 'Name' : 'ETF'}).set_index('ETF코드'), how = 'inner')
 
     tmp.columns = ['종목명', '기준일 비중', '비교일 비중', '차이', 'ETF']

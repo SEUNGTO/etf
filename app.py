458b7a550334fed70f3153af2f8ce8520e871640
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-30 20:46:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -228,8 +228,9 @@ elif search and type == 'Stock' :
 
     with tab1 :
         if research.shape[0] > 0 : 
+            
             tmp = research.set_index('종목명').drop(['종목코드', 'nid'], axis = 1).sort_values('게시일자', ascending = False)
-            st.write(f' 총 **{len(tmp["목표가"])}**개의 리포트가 있어요.')
+            st.write(f' 최근 6개월동안, 총 **{len(tmp["목표가"])}**개의 리포트가 나왔어요.')
 
             st.write(f' 증권사의 평균 목표가는 **{tmp["목표가"].mean():,.0f}**원이에요.')
             st.write(f'- 가장 높은 목표가는 {tmp[tmp["목표가"] == tmp["목표가"].max()]["증권사"].values[0]}의 {tmp["목표가"].max():,.0f}원이에요.')

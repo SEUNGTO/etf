3f1eab24ad37ddfe3b570ca4e6aeadd36107ff6d
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-30 23:20:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -313,9 +313,10 @@ elif search and type == 'Stock' :
         xaxis=dict(type='category', tickangle=45),
         xaxis_rangeslider_visible=False
     )
+    st.write('반영 여부 확인 중')
     url = 'https://raw.githubusercontent.com/SEUNGTO/ETFdata/main/ewm_data.json'
     ewm_data = response.get(url).json()
-    ewm_data = ewm_data[code]
+    ewm_data = ewm_data.loc[one_month_ago:today, code]
     st.dataframe(ewm_data)
     # fig.add_trace(px.line(data, x='Date', y='Volume').data[0])
 

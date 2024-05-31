f8de12cfd81680b93a12caab19dceef5f0af43f3
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-05-31 14:12:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -212,11 +212,38 @@ if search and type == 'ETF':
     similar_etf = load_similar_etf(code)
     col1, col2 = st.columns(2)
     with col1 :
-        st.write(similar_etf)
-
+        st.plotly_chart(fig
+                        , theme="streamlit"
+                        , use_container_width=True)
+        with st.expander(f'{name}의 보유종목 한 눈에 보기') :
+            st.dataframe(df[['종목명', '비중', '평가금액']].sort_values('비중', ascending=False)
+                        ,hide_index = True
+                        ,use_container_width=True)
     with col2 :
-        comp_nm = [codeList.loc[codeList['Symbol'] == comp_code, "Name"] for comp_code in similar_etf]
-        st.write(comp_nm)
+        comp_nm_list = [codeList.loc[codeList['Symbol'] == comp_code, "Name"].values[0] for comp_code in similar_etf]
+        for tab, comp_code in zip(st.tabs(comp_nm_list), similar_etf) :
+            with tab :
+                comp_df = load_etf_data('new', comp_code)  
+                comp_df['비중'] = round(comp_df['비중'], 2)
+                comp_ratio = comp_df.sort_values('비중', ascending=False)[['종목명', '비중']].head(10)
+                comp_ratio.loc['other', :] = ['기타', 100 - sum(comp_ratio['비중'].astype(float))]
+
+                fig = px.pie(comp_ratio, values='비중', names='종목명')
+                fig.update_layout(template='plotly_white'
+                                  , margin={'t': 10, 'b': 5})
+                st.plotly_chart(fig
+                                , theme="streamlit"
+                                , use_container_width=True)
+                with st.expander(f'보유종목 한 눈에 보기') :
+                    st.dataframe(df[['종목명', '비중', '평가금액']].sort_values('비중', ascending=False)
+                                ,hide_index = True
+                                ,use_container_width=True)
+                        
+                ############################################
+                # 상위 10
+                ############################################
+
+                
     
     with st.expander('모든 ETF종목 비교') :
         entire = merge_data(type_dict)

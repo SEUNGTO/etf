3e111f40c9779d999a8344a724288b5c53f61ee9
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-06-02 16:36:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -151,10 +151,10 @@ if search and type == 'ETF':
 
     etf_target = None
     etf_target = load_etf_target_price(code)
-    st.write([idx[-5:] for idx in etf_target.index])
 
     if etf_target is not None :
         etf_target = etf_target[etf_target.index >= one_month_ago]
+        st.dataframe(etf_target)
         fig.add_trace(go.Scatter(
                 x = [idx[-5:] for idx in etf_target.index],
                 y = standardize(etf_target).values,
